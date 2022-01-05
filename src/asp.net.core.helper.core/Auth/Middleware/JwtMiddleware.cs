52c5b9ec3a97b5b992c80b498633ab83c96045f1
bernhard <bernhard@pollerspoeck.net>
2022-01-05 06:04:36 +0100

    IsUserAuthenticationAllowed added HttpContext

--- src/asp.net.core.helper.core/Auth/Middleware/JwtMiddleware.cs
+++ src/asp.net.core.helper.core/Auth/Middleware/JwtMiddleware.cs
@@ -32,7 +32,7 @@ namespace asp.net.core.helper.core.Middleware
             if (result.Succes 
                 && result.Username is not null
                 && result.UserId is not null
-                && authenticationConfiguration.IsUserAuthenticationAllowed(result.UserId, result.Username))
+                && authenticationConfiguration.IsUserAuthenticationAllowed(result.UserId, result.Username, context))
             {
                 context.Items[Constants.KEY_USER_ID] = result.UserId;
                 context.Items[Constants.KEY_TOKEN_VALID] = true;
